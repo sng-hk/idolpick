94780da788c7a78ea4586ad7e97586b9d7eeba45
sng-hk <csh091456743@gmail.com>
2025-06-08 13:41:33 +0900

    feat: add save user method on userrepository

--- src/main/java/com/example/idolpick/entity/User.java
+++ src/main/java/com/example/idolpick/entity/User.java
@@ -1,11 +1,27 @@
 package com.example.idolpick.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
+    private Long id;
     private String email;
     private String nickname;
     private String phoneNumber;
     private String birthDate;
+
+    // 회원가입용 생성자
+    public User(String email, String nickname, String phoneNumber, String birthDate) {
+        this.email = email;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.birthDate = birthDate;
+    }
 }
+
--- src/main/java/com/example/idolpick/repository/UserRepository.java
+++ src/main/java/com/example/idolpick/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.example.idolpick.repository;
 
 import com.example.idolpick.entity.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Optional;
 
 @Repository
@@ -15,15 +20,40 @@ public class UserRepository {
     }
 
     public Optional<User> findByEmail(String email) {
+        // 이메일이 없는 경우가 자연스럽기 때문에 try catch로 감싸서 처리
         String sql = "SELECT email, nickname, phone_number, birth_date FROM user WHERE email = ?";
-        User user = jdbcTemplate.queryForObject(sql,
-                (rs, rowNum) -> new User(
-                        rs.getString("email"),
-                        rs.getString("nickname"),
-                        rs.getString("phone_number"),
-                        rs.getString("birth_date")
-                ), email);
-        return Optional.ofNullable(user);
+        try {
+            User user = jdbcTemplate.queryForObject(sql,
+                    (rs, rowNum) -> new User(
+                            rs.getLong("id"),
+                            rs.getString("email"),
+                            rs.getString("nickname"),
+                            rs.getString("phone_number"),
+                            rs.getString("birth_date")
+                    ), email);
+            return Optional.ofNullable(user);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+
+    }
+
+    public User save(User user) {
+        String sql = "INSERT INTO user (email, nickname, phone_number, birth_date) VALUES (?, ?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setString(2, user.getNickname());
+            ps.setString(3, user.getPhoneNumber());
+            ps.setString(4, user.getBirthDate());
+            return ps;
+        }, keyHolder);
+
+        Long generatedId = keyHolder.getKey().longValue();
+        return new User(generatedId, user.getEmail(), user.getNickname(), user.getPhoneNumber(), user.getBirthDate());
     }
 
 }
